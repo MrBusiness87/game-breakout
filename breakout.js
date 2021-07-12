f8ca26d47cd00a2e1d861bdbc84975c682908ce4
MrBusiness87 <bbartula87@gmail.com>
2021-07-11 23:23:34 -0400

    game-breakout

--- breakout.js
+++ breakout.js
@@ -2,6 +2,8 @@ var breakout = document.getElementById("breakout");
 var ctx = breakout.getContext("2d");
 var x = breakout.width/2;
 var y = breakout.height-30;
+var dx = 2;
+var dy = -2;
 
 // darkgreen square
 ctx.beginPath();
