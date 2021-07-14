c78a1cbae43a112feb2a0c970bceccb607bb9773
MrBusiness87 <bbartula87@gmail.com>
2021-07-13 22:22:26 -0400

    game-breakout

--- breakout.js
+++ breakout.js
@@ -5,36 +5,43 @@ var y = breakout.height-30;
 var dx = 2;
 var dy = -2;
 
-// darkgreen square
-game.beginPath();
-game.rect(20,40,50,50);
-game.fillStyle = "darkgreen";
-game.fill(); //can be replace for game.stroke();
-game.closePath();
+// // darkgreen square
+// game.beginPath();
+// game.rect(20,40,50,50);
+// game.fillStyle = "darkgreen";
+// game.fill(); //can be replace for game.stroke();
+// game.closePath();
 
-// green circle
-game.beginPath();
-game.arc(240, 160, 20, 0, Math.PI*2, false);
-game.fillStyle = "green";
-game.fill(); //can be replace for game.stroke();
-game.closePath();
+// // green circle
+// game.beginPath();
+// game.arc(240, 160, 20, 0, Math.PI*2, false);
+// game.fillStyle = "green";
+// game.fill(); //can be replace for game.stroke();
+// game.closePath();
 
-// empty rectangle for game
-game.beginPath();
-game.rect(160, 10, 100, 40);
-game.strokeStyle = "blue";
-game.stroke();
-game.closePath();
+// // empty rectangle for game
+// game.beginPath();
+// game.rect(160, 10, 100, 40);
+// game.strokeStyle = "blue";
+// game.stroke();
+// game.closePath();
 
-function draw() {
-  // drawing code
-  game.clearRect(0, 0, breakout.width, breakout.height)
+// draws the element
+function ball() {
   game.beginPath();
   game.arc(x, y, 10, 0, Math.PI*2);
   game.fillStyle = "turquoise";
   game.fill();
   game.closePath();
+}
+
+// drawing code
+function draw() {
+  // clears the frame
+  game.clearRect(0, 0, breakout.width, breakout.height)
+  ball();
   x += dx;
   y += dy;
 }
+
 setInterval(draw, 10);
